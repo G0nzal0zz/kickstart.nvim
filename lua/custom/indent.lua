f5dd8fda44ca002da22a22dbcffd8bc5b2f847fe
gonzalo <larroyagonzalo+git@gmail.com>
2026-02-15 12:46:16 +0000

    change indent to 2 spaces

---
+++ lua/custom/indent.lua
@@ -0,0 +1,4 @@
+vim.o.expandtab = true -- expand tab input with spaces characters
+vim.o.smartindent = true -- syntax aware indentations for newline inserts
+vim.o.tabstop = 1 -- num of space characters per tab
+vim.o.shiftwidth = 1 -- spaces per indentation level
